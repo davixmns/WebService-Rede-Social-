d6c3644e671cf58c4ca26c8ecc79726e1a5a16ff
Davi Ximenes <davixmnsl@gmail.com>
2023-01-16 17:37:07 -0300

    primeiro c

---
+++ src/main/java/com/davixmns/workshopmongodb/WorkShopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.davixmns.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShopMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davixmns/workshopmongodb/WorkShopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davixmns.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
