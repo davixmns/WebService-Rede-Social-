c10929c7d71bb129a7cc6076a372f487f4adb819
Davi Ximenes <davixmnsl@gmail.com>
2023-01-19 17:40:24 -0300

    findbyid

--- src/main/java/com/davixmns/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/davixmns/workshopmongodb/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.davixmns.workshopmongodb.dto.UserDTO;
 import com.davixmns.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,11 @@ public class UserResource {
         List<User> users = service.findAll();
         List<UserDTO> userDTOS = users.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(userDTOS);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
