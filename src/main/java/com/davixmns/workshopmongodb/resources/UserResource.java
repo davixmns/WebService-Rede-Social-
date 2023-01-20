0577a971b1052fc49e079ca4b40db5728f4eaec5
Davi Ximenes <davixmnsl@gmail.com>
2023-01-20 18:16:38 -0300

    user (put delete insert)

--- src/main/java/com/davixmns/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/davixmns/workshopmongodb/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.davixmns.workshopmongodb.dto.UserDTO;
 import com.davixmns.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,4 +29,27 @@ public class UserResource {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+        User user = service.fromDTO(objDTO);
+        service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objDTO.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDTO){
+        User user = service.fromDTO(userDTO);
+        user.setId(id);
+        service.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/davixmns/workshopmongodb/services/UserService.java
+++ src/main/java/com/davixmns/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.davixmns.workshopmongodb.services;
 
 import com.davixmns.workshopmongodb.domain.User;
+import com.davixmns.workshopmongodb.dto.UserDTO;
 import com.davixmns.workshopmongodb.repository.UserRepository;
 import com.davixmns.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,41 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public void deleteById(String id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+    public User update(User obj) {
+        Optional<User> newObj = repository.findById(obj.getId());
+        if (newObj.isPresent()) {
+            updateData(newObj.get(), obj);
+            return repository.save(newObj.get());
+        } else{
+            throw new ObjectNotFoundException("Object not found");
+        }
+    }
+
+    public void updateData(User newObj, User obj){
+        newObj.setEmail(obj.getEmail());
+        newObj.setName(obj.getName());
+    }
+
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
